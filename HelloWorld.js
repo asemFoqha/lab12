00a293206aba09ecb6933fed9e9dcb69da6d4a0a
asemFoqha <asem.sh.foqha@gmail.com>
2023-05-16 01:08:34 -0500

    fix

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,33 +1,4 @@
-// var http = require("http");
-// var dt = require("./myModule");
-
-// // http
-// //   .createServer(function (req, res) {
-// //     res.writeHead(200, { "Content-Type": "text/html" });
-// //     res.write("miu asem");
-// //     res.write("The date and time are currently: " + dt.myDate());
-// //     res.end();
-// //   })
-// //   .listen(8080);
-
-// // var http = require("http");
-// // http
-// //   .createServer(function (req, res) {
-// //     res.writeHead(200, { "Content-Type": "text/html" });
-// //     res.write(req.url);
-// //     res.end();
-// //   })
-// //   .listen(8080);
-
-// var url = require("url");
-// http
-//   .createServer(function (req, res) {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     var q = url.parse(req.url, true).query;
-//     var txt = q.year + " " + q.month;
-//     res.end(txt);
-//   })
-//   .listen(8080);
+//exercise n1
 
 // var http = require("http");
 // http
@@ -36,6 +7,8 @@
 //     res.end("Hello World!");
 //   })
 //   .listen(8080);
+
+//exercise n2
 
 // var http = require("http");
 // var dt = require("./myModule");
@@ -47,6 +20,7 @@
 //   })
 //   .listen(8080);
 
+//exercise n3
 var http = require("http");
 http
   .createServer(function (req, res) {
